d625140e45919a68e71a3eb4602ea7c6b6b30891
FliiFe <theophile.cailliau@gmail.com>
2017-11-16 20:41:17 +0100

    Support multiple versions of pbm

--- scripts/parsescan.sh
+++ scripts/parsescan.sh
@@ -1,5 +1,7 @@
 #!/bin/fish
 
+command -v pnmtoplainpnm ^/dev/null >/dev/null; or alias pnmtoplainpnm="pnmtopnm -plain"
+
 set left_elems 35
 set total_v 43
 set top_right_elems (echo $total_v-$left_elems | bc)
@@ -54,7 +56,7 @@ set percentage (echo 100/$left_elems | bc -l)
 
 for i in (seq 1 $left_elems)
     set offset (echo "$height*($i-1)*$percentage/100" | bc -l | cut -d. -f1)
-    set ones (convert left.jpg -crop 100%x$percentage%+0+$offset +repage pbm:- | pnmtopnm -plain | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
+    set ones (convert left.jpg -crop 100%x$percentage%+0+$offset +repage pbm:- | pnmtoplainpnm | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
     # TODO: use a treshold relative to part width and height.
     test "$ones" -gt 70; and echo "v$i yes"; or echo "v$i no"
 end
@@ -83,7 +85,7 @@ rm right.jpg
 
 for i in (seq 1 $top_right_elems)
     set offset (echo "($i-1)*$cellheight" | bc)
-    set ones (convert righttop.jpg -crop $rightwidth'x'$cellheight+0+$offset +repage pbm:- | pnmtopnm -plain | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
+    set ones (convert righttop.jpg -crop $rightwidth'x'$cellheight+0+$offset +repage pbm:- | pnmtoplainpnm | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
     set pos (echo $i+$left_elems | bc)
     test "$ones" -gt 70; and echo "v$pos yes"; or echo "v$pos no"
 end
@@ -93,7 +95,7 @@ rm righttop.jpg
 
 for i in (seq 1 $total_b)
     set offset (echo "($i-1)*$cellheight" | bc)
-    set ones (convert rightbot.jpg -crop $rightwidth'x'$cellheight+0+$offset +repage pbm:- | pnmtopnm -plain | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
+    set ones (convert rightbot.jpg -crop $rightwidth'x'$cellheight+0+$offset +repage pbm:- | pnmtoplainpnm | tail -n 21 | tr -d '\n' | sed s/0//g | wc -c)
     test "$ones" -gt 70; and echo "b$i yes"; or echo "b$i no"
 end
 
